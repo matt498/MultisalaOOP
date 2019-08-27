506023fe55898827e36b6a705c72682103e5064b
matteoferrari <matteoferrari@mbp-di-matteo.homenet.telecomitalia.it>
2019-08-27 22:10:12 +0200

    corretto bug

--- Multisala/src/gui/CheckOutPanel.java
+++ Multisala/src/gui/CheckOutPanel.java
@@ -60,6 +60,10 @@ public class CheckOutPanel extends JPanel {
 		okButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				
+				if(getResPanel().getProEvent() == null) {
+					return;
+				}
 				try {
 					controller.checkOutTickets(getResPanel().getProEvent());
 				} catch (SQLException e1) {
